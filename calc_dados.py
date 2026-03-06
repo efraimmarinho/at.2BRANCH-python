c4868065c07ece01e2a630dcaa197bcefb6c0c64
EFRAIM PINOTTI MARINHO <47804881843@info.local>
2026-03-06 18:49:31 -0300

    feat: estrutura inicial e validacao

---
+++ calc_dados.py
@@ -0,0 +1,10 @@
+def validar_vetores(v1, v2):
+    if len(v1) != len(v2):
+        raise ArithmeticError ("Os vetores tdevem ter os mesmos tamanhos")
+    return True
+
+if __name__ == "__main__":
+    obs = [1.2, 2.5, 3.8]
+    pred = [1.0, 2.7, 4.0]
+
+print("Sistema inicializado com sucesso!")
